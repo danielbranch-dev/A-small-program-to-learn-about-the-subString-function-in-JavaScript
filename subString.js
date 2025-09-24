d27db1dfc9429ac90f1a16e6cb45c8940df3aedf
danielbranch-dev <daniel.branchdev@gmail.com>
2025-09-24 19:35:24 -0400

    Update subString.js

--- subString.js
+++ subString.js
@@ -3,6 +3,6 @@ let enterNumber = prompt("Enter a 7 digit number");
 alert ('We are going to return the substring of the 2nd and 6th index');
 let returnedSubstring = enterNumber.substring(2,6);
 
-let message = `In order to learn about the subString function in JavaScript, you entered the 7 digit number ${enterNumber}. The subString function includes the indexes of 2 and 6 because we specified it to do so in our program. The substring funvtion works by returning the "2nd index", "6th index" and the characters between the "2nd" and "6th" index So the returnedSubstring final output is ${returnedSubstring}`;
+let message = `In order to learn about the subString function in JavaScript, you entered the 7 digit number ${enterNumber}. The subString function includes the indexes of 2 and 6 because we specified it to do so in our program. The substring function works by returning the "2nd index", "6th index" and the characters between the "2nd" and "6th" index So the returnedSubstring final output is ${returnedSubstring}`;
 
 alert(message);
